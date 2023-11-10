291c2e6051eda312f8f017bbe06d4b5b46509394
Patricia <kovach.patri@gmail.com>
2023-11-10 19:47:25 +0100

    computing values & properly handling number values

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,3 +1,9 @@
+import { calculateInvestmentResults } from "../util/investment"
+
 export default function Results({ input }) {
+    const resultsData = calculateInvestmentResults(input);
+
+    console.log(resultsData);
+
     return <p>Results...</p>
 }
